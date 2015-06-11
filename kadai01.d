694ed6abfdac0b7e880fe92f292caea4959a2801
Takehiro Arai <takenta0920@outlook.com>
2015-06-11 23:00:08 +0900

    Complete make a method getPathList

--- kadai01.d
+++ kadai01.d
@@ -4,39 +4,38 @@ import std.algorithm;
 import std.conv;
 
 void main() {
+    auto fin = File("arc_list.txt");
     int[][] arc_info = [];
     auto start_point = 0;
 
     //Arc情報の読み込み
-    foreach(input; stdin.byLine) {
+    foreach(input; fin.byLine) {
         int[] line;
         foreach(num; input.split(" ")) {
             line ~= num.to!int;
         }
         arc_info ~= line;
+        line.writeln;
     }
 
-    auto path_list = getPath(arc_info, [], [], start_point, start_point);
-    writeln("path_list:", path_list);
+    int i = 0;
+    foreach(path; getPathList(arc_info.length.to!int, [start_point])) {
+        path.writeln;
+        i++;
+    }
+    i.writeln;
 }
 
-int[][] getPath(int[][] arc_info, int[][] path_list, int[] path, int start_point, int end_point) {
-    if (path.length == arc_info.length) {
-        path ~= end_point;
-        path_list ~= path;
+int[][] getPathList(int num_points, int[] route) {
+    int[][] path_list = [];
 
-        writeln("path: ", path);
-        writeln("=> this route get a goal");
-        return path_list;
+    if (route.length >= num_points) {
+        return (path_list ~ (route ~ 0));
     }
 
-    foreach(next_points; arc_info[start_point]) {
-        foreach(next_point; 0..arc_info.length) {
-            // まだpathに登録されていなかったら、登録して再帰呼出し
-            if (!canFind(path, next_point)) {
-                path ~= next_point.to!int; path.writeln;
-                path_list ~= getPath(arc_info, path_list, path.dup, next_point.to!int, end_point);
-            }
+    foreach(now_point; 0..num_points) {
+        if(!canFind(route, now_point)) {
+            path_list ~= getPathList(num_points, route ~ now_point);
         }
     }
 
