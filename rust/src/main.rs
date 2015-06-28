c0734cdd933fafa5ab252b1210457203c707945f
takenta <takenta0920@outlook.com>
2015-06-29 02:32:42 +0900

    Add nearest-addition method to rust-version

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -7,13 +7,21 @@ fn main() {
                         vec![4, 3, 4, 0, 1, 4],
                         vec![6, 3, 3, 1, 0, 5],
                         vec![1, 3, 9, 4, 5, 0]];
+    let start_point: usize = 0;
 
-    let test_list = path_list::PathList::new(&arc_info);
+    let test_list = path_list::PathList::new(&arc_info, start_point);
 
-    print!("===============\nAll Path List\n");
+    println!("===============");
+    println!("All Path List");
     println!("{}", test_list);
 
-    let optimal_path = test_list.all_enumerate();
-    print!("===============\nAll Enumerate Method\n");
+    let mut optimal_path = test_list.all_enumerate();
+    println!("===============");
+    println!("All Enumerate Method");
+    println!("Optimal Path: [{}] ({})", optimal_path, optimal_path.cost());
+
+    optimal_path = test_list.generate_optimal("NA");
+    println!("===============");
+    println!("Nearest Addtion Method");
     println!("Optimal Path: [{}] ({})", optimal_path, optimal_path.cost());
 }
--- rust/src/path_list.rs
+++ rust/src/path_list.rs
@@ -7,15 +7,15 @@ pub struct SalesmanPath {
 }
 
 impl SalesmanPath {
-    pub fn new(arc_info: &Vec<Vec<usize>>, start_point: usize) -> SalesmanPath {
+    pub fn new(arc_info: &Vec<Vec<usize>>, start_node: usize) -> SalesmanPath {
         SalesmanPath {
             arc_info: arc_info.clone(),
-            salesman_path: vec![start_point],
+            salesman_path: vec![start_node],
         }
     }
 
-    pub fn add(&mut self, node: usize) {
-        self.salesman_path.push(node);
+    pub fn path(&self) -> Vec<usize> {
+        self.salesman_path.clone()
     }
 
     pub fn cost(&self) -> usize {
@@ -24,6 +24,18 @@ impl SalesmanPath {
             sum += self.arc_info[win[0]][win[1]];
         }
         return sum;
+    }
+
+    pub fn len(&self) -> usize {
+        self.salesman_path.len()
+    }
+
+    pub fn add(&mut self, node: usize) {
+        self.salesman_path.push(node);
+    }
+
+    pub fn insert(&mut self, index: usize, element: usize) {
+        self.salesman_path.insert(index, element);
     }
 
     pub fn close(&mut self) -> SalesmanPath {
@@ -49,18 +61,20 @@ impl fmt::Display for SalesmanPath {
 
 pub struct PathList {
     arc_info: Vec<Vec<usize>>,
+    start_node: usize,
 }
 
 impl PathList {
-    pub fn new(arc_info: &Vec<Vec<usize>>) -> PathList {
+    pub fn new(arc_info: &Vec<Vec<usize>>, start_node: usize) -> PathList {
         PathList {
-            arc_info: arc_info.clone()
+            arc_info: arc_info.clone(),
+            start_node: start_node
         }
     }
 
-    pub fn generate(&self, prev_path: &mut SalesmanPath, unused_nodes: &mut Vec<usize>) -> Vec<SalesmanPath> {
+    pub fn generate_all(&self, prev_path: &mut SalesmanPath, unused_nodes: &Vec<usize>) -> Vec<SalesmanPath> {
         let mut buffer:Vec<SalesmanPath> = Vec::new();
-        match (*unused_nodes).is_empty() {
+        match unused_nodes.is_empty() {
             true => {
                 buffer.push(prev_path.close());
             },
@@ -75,8 +89,8 @@ impl PathList {
                     let mut nodes = unused_nodes.clone();
                     nodes.retain(|&x| x != *node);
 
-                    // Call self.generate(prev_path, unused_nodes) by the path and the array.
-                    let list = self.generate(&mut path, &mut nodes);
+                    // Call self.generate_all(prev_path, unused_nodes) by the path and the array.
+                    let list = self.generate_all(&mut path, &mut nodes);
                     for temp in list {
                         buffer.push(temp);
                     }
@@ -86,23 +100,83 @@ impl PathList {
         buffer.clone()
     }
 
+    pub fn generate_optimal(&self, mode: &str) -> SalesmanPath {
+        match mode {
+            "AE" => {
+                self.all_enumerate().clone()
+            },
+            "NA" => {
+                // Create unused_nodes.
+                let mut unused_nodes = self.nodes();
+                unused_nodes.retain(|&x| x != self.start_node);
+
+                self.nearest_addition(&mut SalesmanPath::new(&self.arc_info, self.start_node).close(), &unused_nodes).clone()
+            },
+            _ => {
+                println!("The method don't exists");
+                SalesmanPath::new(&self.arc_info, self.start_node).clone()
+            }
+        }
+    }
+
     pub fn all_enumerate(&self) -> SalesmanPath {
-        // Get list that contained all paths.
-        let list = self.generate(&mut SalesmanPath::new(&self.arc_info, 0), &mut vec![1,2,3,4,5]);
+        // Create unused_nodes.
+        let mut unused_nodes = self.nodes();
+        unused_nodes.retain(|&x| x != self.start_node);
+
+        // Create list that contained all paths.
+        let path_list = self.generate_all(&mut SalesmanPath::new(&self.arc_info, self.start_node), &unused_nodes);
 
         // Search a optimal-path in the list.
         // memo) max_by() is unstable.
-        let mut min = list.first().unwrap();
-        for path in list.iter() {
+        let mut min = path_list.first().unwrap();
+        for path in path_list.iter() {
             if min.cost() > path.cost() { min = path; }
         }
         min.clone()
+    }
+
+    pub fn nearest_addition(&self, prev_path: &mut SalesmanPath, unused_nodes: &Vec<usize>) -> SalesmanPath {
+        match unused_nodes.is_empty() {
+            true => {
+                prev_path.clone()
+            },
+            false => {
+                let path = prev_path.path();
+                let mut min_arc = (0, path.first().unwrap(), unused_nodes.first().unwrap());
+
+                // take out a node from `unused_nodes`
+                for (index, prev_node) in path.iter().enumerate() {
+                    for next_node in unused_nodes.iter() {
+                        if self.arc_info[*prev_node][*next_node] < self.arc_info[*min_arc.1][*min_arc.2] {
+                            min_arc = (index, prev_node, next_node);
+                        }
+                    }
+                }
+
+                let mut path = prev_path.clone();
+                path.insert(min_arc.0 + 1, *min_arc.2);
+
+                let mut nodes = unused_nodes.clone();
+                nodes.retain(|&x| x != *min_arc.2);
+
+                self.nearest_addition(&mut path, &nodes)
+            }
+        }
+    }
+
+    fn nodes(&self) -> Vec<usize> {
+        let mut nodes: Vec<usize> = Vec::new();
+        for node in 0..self.arc_info.len() {
+            nodes.push(node);
+        }
+        nodes.clone()
     }
 }
 
 impl fmt::Display for PathList {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let list = self.generate(&mut SalesmanPath::new(&self.arc_info, 0), &mut vec![1,2,3,4,5]);
+        let list = self.generate_all(&mut SalesmanPath::new(&self.arc_info, 0), &mut vec![1,2,3,4,5]);
         let len = list.len();
 
         for (count, path) in list.iter().enumerate() {
