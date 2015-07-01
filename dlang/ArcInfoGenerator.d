f8c46ac60140ad0d344be82f20f8a14252aef1a6
takenta <takenta0920@outlook.com>
2015-07-01 10:56:07 +0900

    Modified ArcInfoGenerator and the outputs

--- dlang/ArcInfoGenerator.d
+++ dlang/ArcInfoGenerator.d
@@ -12,12 +12,21 @@ void main(string[] args) {
 
     foreach (i; 0..num_node) {
         foreach (j; i..num_node) {
+            /* i==j の時、つまりは今いる点に移動する場合のコストは 0 */
             if (i == j) {
                 arc_info[i][j] = 0;
                 continue;
             }
 
-            arc_info[i][j] = uniform(1, 10, seed);
+            /* 最大値は10未満 */
+            /* さらに既に設定されている数値について、三角不等式を満たす */
+            auto limit = 10L;
+            foreach (k; 0..i.to!uint) {
+                auto cost = arc_info[i][k] + arc_info[k][j];
+                if (cost < limit) limit = cost;
+            }
+
+            arc_info[i][j] = uniform(1, limit, seed);
             arc_info[j][i] = arc_info[i][j];
         }
     }
